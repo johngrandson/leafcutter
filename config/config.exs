09d2908352a44c721e70af0b051e993e39db1cf4
João Netto <joaonettopb@hotmail.com>
2026-08-26 13:38:03 -0300

    Configure durable runtime node heartbeat mode

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,8 @@ config :logger, :default_formatter,
 config :phoenix, :json_library, Jason
 
 config :leafcutter_runtime, LeafcutterRuntime.NodeHeartbeat,
-  interval: 15_000
+  interval: 15_000,
+  mode: :durable
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
