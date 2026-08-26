f58d2dfb3a00b0e6c6897b42617c63096a5cf53b
João Netto <joaonettopb@hotmail.com>
2026-08-26 14:54:37 -0300

    Query the PostgreSQL clock through the shared Repo

--- apps/leafcutter_runtime/lib/leafcutter/executions/database_clock.ex
+++ apps/leafcutter_runtime/lib/leafcutter/executions/database_clock.ex
@@ -1,14 +1,13 @@
 defmodule Leafcutter.Executions.DatabaseClock do
   @moduledoc false
 
-  alias Ecto.Adapters.SQL
   alias Leafcutter.Repo
 
   @doc false
   @spec now() :: DateTime.t()
   def now do
     %{rows: [[%DateTime{} = timestamp]]} =
-      SQL.query!(Repo, "SELECT clock_timestamp()", [])
+      Repo.query!("SELECT clock_timestamp()", [])
 
     timestamp
   end
